0528d9f7f27a0d18cb04ffadbaceb796d1a2f9bc
LarsCampus19 <Larsp.jobs@gmail.com>
2023-10-05 23:40:58 +0200

    missed the update on the lexer(auto_push)

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: wmarien <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 12:29:56 by wmarien           #+#    #+#             */
-/*   Updated: 2023/10/05 20:16:45 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/10/05 23:40:31 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,29 @@
 
 int	lexer(char *line)
 {
-	int			err;
-	t_token		*token_lst;
+	int		err;
+	t_token	*token_lst;
+	t_token	*curr;
 
 	err = 0;
 	token_lst = NULL;
 	while (*line)
 	{
 		if (err)
-			return (lexer_err(&token_lst, NULL,
-					"\033[0;31mlexer failed\033[0;39m"));
-		while (ft_isspace(*line))
+			return (lexer_err(&token_lst, NULL, "lexer failed"));
+		while (*line && ft_isspace(*line))
 			(*line)++;
-		if (is_seperator(line))
+		if (!ft_strncmp(line, "<", 1) || !ft_strncmp(line, ">", 1)
+			|| !ft_strncmp(line, "|", 1))
 			err = !handle_seperator(&line, &token_lst);
 		else
 			err = !handle_identifier(&line, &token_lst);
+	}
+	curr = token_lst;
+	while (curr)
+	{
+		printf("Type: %d\nValue: %s\n", curr->type, curr->value);
+		curr = curr->next;
 	}
 	return (0);
 }
