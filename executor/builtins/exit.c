a6b8ac160b77867511748742ff0c1d9a32ad3c74
LarsCampus19 <Larsp.jobs@gmail.com>
2023-11-21 12:25:55 +0100

    fixed exit to also take exit statuses(auto_push)

--- executor/builtins/exit.c
+++ executor/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: lpeeters <lpeeters@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 01:12:17 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/11/20 23:49:22 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/11/21 12:24:05 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,28 @@
 //exit the shell in a clean way
 int	exec_exit(void)
 {
-	if (g_minishell.ast->args[4] != '\0' && g_minishell.ast->args[4] == ' ')
+	int	status;
+	int	i;
+
+	if (g_minishell.ast->args[4] != '\0' && g_minishell.ast->args[4] != ' ')
+		return (g_minishell.exit_code = 127,
+			prnt_err("exit: invalid usage", NULL));
+	if (g_minishell.ast->args[4] == ' ' && g_minishell.ast->args[5] == '-')
 		return (prnt_err("exit: invalid usage", NULL));
-	if (g_minishell.ast->args[4] != '\0')
-		return (g_minishell.exit_code = 127, 1);
+	status = EXIT_SUCCESS;
+	if (g_minishell.ast->args[4] == ' ')
+	{
+		i = 4;
+		while (g_minishell.ast->args[++i])
+			if (!ft_isdigit(g_minishell.ast->args[i]))
+				return (prnt_err("exit: invalid usage", NULL));
+		status = ft_atoi(g_minishell.ast->args + 5);
+	}
 	free_token_lst(&g_minishell.tokens);
 	free_lst(&g_minishell.exp_env);
 	free_lst(&g_minishell.var_lst);
 	free_ast_nodes(g_minishell.ast);
 	rl_clear_history();
-	exit(EXIT_SUCCESS);
+	exit(status);
 	return (0);
 }
